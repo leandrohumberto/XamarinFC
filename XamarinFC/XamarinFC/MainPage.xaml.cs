7e7940d5ef0815bc91ebc5c3c7d305dcf699ef50
Leandro Humberto <lehumberto@msn.com>
2017-06-07 21:55:48 -0300

    Add project files.

---
+++ XamarinFC/XamarinFC/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
