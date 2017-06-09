4949f0580904ded2090d9024de3b76c688329766
Leandro Humberto <leandrohumberto@gmail.com>
2017-06-09 02:56:30 -0300

    Inclusão de novos hinos na classe OfflineFootballClubRepository

--- XamarinFC/XamarinFC/Model/Repository/OfflineFootballClubRepository.cs
+++ XamarinFC/XamarinFC/Model/Repository/OfflineFootballClubRepository.cs
@@ -78,25 +78,138 @@ namespace XamarinFC.Model.Repository
                 new FootballClub
                 {
                     Name = "São Paulo",
-                    Anthem = "GAAAAAAAAAAAAAAAAAAAAAAAAYYYYYYYYYYY",
+                    Anthem =
+                        "Salve o tricolor paulista\n" +
+                        "Amado clube brasileiro\n" +
+                        "Tu és forte, tu és grande\n" +
+                        "Dentre os grandes és o primeiro\n" +
+                        "Tu és forte, tu és grande\n" +
+                        "Dentre os grandes és o primeiro\n" +
+                        "\n" +
+                        "Ó tricolor\n" +
+                        "Clube bem amado\n" +
+                        "As tuas glórias\n" +
+                        "Vêm do passado\n" +
+                        "(x2)\n" +
+                        "\n" +
+                        "São teus guias brasileiros\n" +
+                        "Que te amam ternamente\n" +
+                        "De São Paulo tens o nome\n" +
+                        "Que ostentas dignamente\n" +
+                        "\n" +
+                        "Coro: Oh tricolor...\n" +
+                        "\n" +
+                        "São Paulo clube querido\n" +
+                        "Tu tens o nosso amor\n" +
+                        "Teu nome e tuas glórias\n" +
+                        "Têm honra e resplendor\n" +
+                        "\n" +
+                        "Coro: Oh tricolor...\n" +
+                        "\n" +
+                        "Tuas cores gloriosas\n" +
+                        "Despertam amor febril\n" +
+                        "Pela terra Bandeirante:\n" +
+                        "Honra e Glória do Brasil\n" +
+                        "\n" +
+                        "Coro: Oh tricolor...\n",
                 },
 
                 new FootballClub
                 {
                     Name = "Santos",
-                    Anthem = "pran7os",
+                    Anthem =
+                        "Sou alvinegro da Vila Belmiro\n" +
+                        "O Santos vive no meu coração\n" +
+                        "É o motivo de todo o meu riso\n" +
+                        "De minhas lágrimas de emoção\n" +
+                        "\n" +
+                        "Sua bandeira no mastro é a história\n" +
+                        "De um passado e um presente só de glórias\n" +
+                        "Nascer, viver, e no Santos morrer\n" +
+                        "É um orgulho que nem todos podem ter\n" +
+                        "\n" +
+                        "No Santos pratica-se o esporte\n" +
+                        "Com dignidade e com fervor\n" +
+                        "Seja qual for a sua sorte\n" +
+                        "De vencido ou de vencedor\n" +
+                        "\n" +
+                        "Com técnica e disciplina\n" +
+                        "Dando o sangue com amor\n" +
+                        "Pela bandeira que ensina\n" +
+                        "Lutar com fé e com ardor\n",
                 },
 
                 new FootballClub
                 {
                     Name = "Palmeiras",
-                    Anthem = "Não tem mundial",
+                    Anthem =
+                        "Quando surge o alviverde imponente\n" +
+                        "No gramado em que a luta o aguarda\n" +
+                        "Sabe bem o que vem pela frente\n" +
+                        "Que a dureza do prélio não tarda\n" +
+                        "\n" +
+                        "E o Palmeiras no ardor da partida\n" +
+                        "Transformando a lealdade em padrão\n" +
+                        "Sabe sempre levar de vencida\n" +
+                        "E mostrar que de fato é campeão\n" +
+                        "\n" +
+                        "Defesa que ninguém passa\n" +
+                        "Linha atacante de raça\n" +
+                        "Torcida que canta e vibra\n" +
+                        "\n" +
+                        "Por nosso alviverde inteiro\n" +
+                        "Que sabe ser brasileiro\n" +
+                        "Ostentando a sua fibra\n",
                 },
 
                 new FootballClub
                 {
                     Name = "Fluminense",
-                    Anthem = "FluminenC",
+                    Anthem =
+                        "Sou tricolor de coração\n" +
+                        "Sou do clube tantas vezes campeão\n" +
+                        "Fascina pela sua disciplina\n" +
+                        "O Fluminense me domina\n" +
+                        "Eu tenho amor ao tricolor\n" +
+                        "\n" +
+                        "Salve o querido pavilhão\n" +
+                        "Das três cores que traduzem tradição\n" +
+                        "A paz, a esperança e o vigor\n" +
+                        "Unido e forte pelo esporte\n" +
+                        "Eu sou é tricolor\n" +
+                        "\n" +
+                        "Vence o Fluminense\n" +
+                        "Com o verde da esperança pois\n" +
+                        "Quem espera sempre alcança\n" +
+                        "Clube que orgulha o Brasil\n" +
+                        "Retumbante de glórias\n" +
+                        "E vitórias mil\n" +
+                        "\n" +
+                        "Sou tricolor de coração\n" +
+                        "Sou do clube tantas vezes campeão\n" +
+                        "Fascina pela sua disciplina\n" +
+                        "O Fluminense me domina\n" +
+                        "Eu tenho amor ao tricolor\n" +
+                        "\n" +
+                        "Salve o querido pavilhão\n" +
+                        "Das três cores que traduzem tradição\n" +
+                        "A paz, a esperança e o vigor\n" +
+                        "Unido e forte pelo esporte\n" +
+                        "Eu sou é tricolor\n" +
+                        "\n" +
+                        "Vence o Fluminense\n" +
+                        "Com sangue de encarnado\n" +
+                        "Com amor e com vigor\n" +
+                        "Faz a torcida querida\n" +
+                        "Vibrar com a emoção\n" +
+                        "Do tricampeão\n" +
+                        "\n" +
+                        "Vence o Fluminense\n" +
+                        "Usando a fidalguia\n" +
+                        "Branco é paz e harmonia\n" +
+                        "Brilha no sol da manhã\n" +
+                        "Ou na luz do refletor\n" +
+                        "Salve o Tricolor\n",
                 },
 
                 new FootballClub
