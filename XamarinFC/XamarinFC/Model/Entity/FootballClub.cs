23e854bc6b38c0d581224dd13509086c3d9698fe
Leandro Santos <leandro.santos@atsnet.com.br>
2017-06-19 17:39:30 -0300

    Classe FootballClubRepository adicionada.

--- XamarinFC/XamarinFC/Model/Entity/FootballClub.cs
+++ XamarinFC/XamarinFC/Model/Entity/FootballClub.cs
@@ -1,9 +1,24 @@
-﻿namespace XamarinFC.Model.Entity
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace XamarinFC.Model.Entity
 {
+    [DataTable("FootballClub")]
     public class FootballClub
     {
+        [Version]
+        public string AzureVersion { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
         public string Name { get; set; }
+
+        [JsonProperty("fullName")]
         public string FullName { get; set; }
+
+        [JsonProperty("anthem")]
         public string Anthem { get; set; }
 
         public override string ToString()
---
+++ XamarinFC/XamarinFC/Model/Repository/FootballClubRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinFC.Model.Entity;
+using XamarinFC.Model.Repository;
+
+//[assembly: Dependency(typeof(FootballClubRepository))]
+namespace XamarinFC.Model.Repository
+{
+    public class FootballClubRepository : IFootballClubRepository
+    {
+        private IMobileServiceClient _client;
+        private IMobileServiceTable<FootballClub> _table;
+        private static readonly string AppUrl = "https://xamarin-marathon-intermediate.azurewebsites.net/";
+
+        public FootballClubRepository()
+        {
+            _client = new MobileServiceClient(AppUrl);
+            _table = _client.GetTable<FootballClub>();
+        }
+
+        public async Task<IEnumerable<FootballClub>> GetClubsData()
+        {
+            return await _table.ToEnumerableAsync();
+        }
+    }
+}
